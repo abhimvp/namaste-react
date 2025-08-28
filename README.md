d329465a70bb9a3a6d54b48f7e0aedf5124c47ac
abhimvp <abhishekreddyboddu1@gmail.com>
2025-08-28 23:53:17 +0530

    Laying the foundation - babel, JSX, React Components, Component Composition, Cross Site Scripting

--- README.md
+++ README.md
@@ -2,6 +2,45 @@
 
 - Akshay Saini The BEST.
 
+## Laying the Foundation - babel, JSX
+
+```text
+let's start from scratch
+Some revision info below
+create React Element - it's not an html element - Behind the scenes - creates an Object
+when we render this element on to the DOM - then it becomes a HTML element
+const heading = React.createElement("h1",{id:"heading"},"Hello World from Namaste React!!!")
+create Root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+render h1 tag
+root.render(heading);
+The above way is s not clean  - syntax is very bad - can we do better on elements - as this is
+not developer friendly - to address this facebook developers came up with a new syntax called JSX
+JSX - javascript syntax to create react elements
+it looks similar to HTML but it's not HTML
+Also react is different and JSX is different - JSX is just a syntax sugar for React.createElement
+let's create a JSX for h1
+const jsxHeading = <h1 id="heading">Hello World from Namaste React!!! using JSX</h1>; - this is not pure js but it is js
+jsxHeading is a React Element
+jsx is not html inside javascript - it's a html like syntax
+From here onwards on we will use JSX to create our React elements not React.createElement.
+JSX - transpiled before it reaches to JS Engine on Browser - transpiling is done by parcel
+- which is a bundler and it's babel job(Part of parcel) to do transpiling
+(JSX - converts into browsers understandable JS code - React.createElement("h1",{id:"heading"},"Hello World from Namaste React!!!"))
+- babel is not created by Facebook
+- babel is converting jsx to react.createElement
+```
+
+- [babel](https://babeljs.io/) a javascript compiler. babel does a lot of transpilation for all types of browser.
+  ![alt text](Images/image.png)
+
+- Extensions to Install via VS Code:
+  - Prettier
+  - Bracket Pair Colorization Toggler
+  - Better Comments
+
+- React components
+
 ## [Parcel](https://parceljs.org/)
 
 - Created a dev build
--- app.js
+++ app.js
@@ -1,14 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xy: "aaa" },
-  "Hello World from Namaste React!!!"
+// React Element
+// Babel transpiles JSX to React.createElement calls
+const jsxHeading = (
+  <h1 id="heading" className="jsx">
+    Namaste React using JSX{" "}
+  </h1>
+); // use () - to write JSX in multiple lines.
+
+// console.log(jsxHeading);
+
+// React Component - class & functional ones
+
+const Title = () => {
+  return (
+    <h1 id="heading" className="title" tabIndex="3">
+      Namaste React using JSX - Functional Component 2 Title
+    </h1>
+  );
+};
+
+// Same as above - we don't have to write return statement - don't panic next time
+// we can write Title component inside HeaderComponent2
+
+const HeadingComponent2 = () => (
+  // this is component composition.
+  <div id="headingfunctionalcomponent2">
+    <Title />
+    <Title></Title> {/** Same as above and it works */}
+    <h1 id="heading" className="jsx">
+      Namaste React using JSX - Functional Component 2
+    </h1>
+  </div>
 );
 
-// console.log(heading);
-// get root element
+const num = 1000000;
+
+// normal javascript variable - react element
+const titleElement = (
+  <h2 id="heading">
+    Namaste React using JSX - Functional Component 2 title react element
+  </h2>
+);
+
+// React Functional component is just a javascript function. Name it with Capital Letter. Which returns some JSX.
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <HeadingComponent2 />
+      {Title()} {/* we can also call component like normal function */}
+      <h1>{num + 356789}</h1>{" "}
+      {/* we can use JS expressions inside JSX using {} */}
+      {/* Even console.log works */}
+      {console.log("Hello World")}
+      {titleElement}
+      {/* As soon as component is rendered the code in {} gets executed. */}
+      <h1 id="heading" className="jsx">
+        
+        Namaste React using JSX - Functional Component
+      </h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// render h1 tag
-root.render(heading);
+// root.render(jsxHeading);
+// This is how you render Functional Components
+root.render(<HeadingComponent />); // syntax babel understands.
